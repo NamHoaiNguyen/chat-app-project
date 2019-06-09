5069dff9f6c94b77df33d9554b694ff95687ded8
unknown <ducphamle212@gmail.com>
2019-06-09 16:54:49 +0700

    fix some small parts

--- backend/controllers/auth/index.js
+++ backend/controllers/auth/index.js
@@ -6,13 +6,8 @@ const { check } = require("express-validator/check");
 
 const router = express.Router();
 
-
 router.post("/login", controller.login);
 
-router.post("/register", check("email").isEmail(),
-  check("password", "Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long")
-    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
-  controller.register);
-
+router.post("/register", check("email").isEmail(), controller.register);
 
 module.exports = router;
